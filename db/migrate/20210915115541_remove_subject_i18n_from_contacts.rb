e74f8f96190ffd4dd30d8f8b0183a87dc91087f9
MASEMIYABI <miyabi.kanjani@ezweb.ne.jp>
2021-09-23 11:03:20 +0000

    ff

--- db/migrate/20210915115541_remove_subject_i18n_from_contacts.rb
+++ db/migrate/20210915115541_remove_subject_i18n_from_contacts.rb
@@ -1,5 +1,5 @@
 class RemoveSubjectI18nFromContacts < ActiveRecord::Migration[5.2]
   def change
-    remove_column :contacts, :integer, :subject_i18n
+    remove_column :contacts, :subject_i18n, :integer
   end
 end
