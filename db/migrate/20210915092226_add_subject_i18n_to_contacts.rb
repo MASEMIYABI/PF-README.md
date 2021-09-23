cf1e3fbf94222f319a357c469c58c4dafa249814
MASEMIYABI <miyabi.kanjani@ezweb.ne.jp>
2021-09-23 11:00:53 +0000

    ff

--- db/migrate/20210915092226_add_subject_i18n_to_contacts.rb
+++ db/migrate/20210915092226_add_subject_i18n_to_contacts.rb
@@ -1,5 +1,5 @@
 class AddSubjectI18nToContacts < ActiveRecord::Migration[5.2]
   def change
-    add_column :contacts, :integer, :subject_i18n
+    add_column :contacts, :subject_i18n, :integer
   end
 end
